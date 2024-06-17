681198409c60f997c441a90e9ef72c03cdbb377e
Ivanzr17 <ivanzafrarendal@gmail.com>
2024-06-17 15:52:49 +0200

    Modificaciones

--- src/js/components/main.jsx
+++ src/js/components/main.jsx
@@ -8,29 +8,43 @@ export class main extends Component {
   render() {
     return (
       <div>
-        <Header />
-        <Jumbotron />
+        <div class="header">
+          <Header />
+        </div>
         <div class="container">
-          <div class="row">
+          <Jumbotron />
+
+          <div class="card-group">
             <Card
               cardTitle={"Titulo 1"}
-              cardText={"Aqui iria el texto de la primera card"}
+              cardText={
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec metus arcu, mattis in porttitor ac, vehicula in libero. Etiam viverra."
+              }
             />
             <Card
               cardTitle={"Titulo 2"}
-              cardText={"Aqui iria el texto de la segunda card"}
+              cardText={
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec metus arcu, mattis in porttitor ac, vehicula in libero. Etiam viverra."
+              }
             />
             <Card
               cardTitle={"Titulo 3"}
-              cardText={"Aqui iria el texto de la tercera card"}
+              cardText={
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec metus arcu, mattis in porttitor ac, vehicula in libero. Etiam viverra."
+              }
             />
             <Card
               cardTitle={"Titulo 4"}
-              cardText={"Aqui iria el texto de la cuarta card"}
+              cardText={
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec metus arcu, mattis in porttitor ac, vehicula in libero. Etiam viverra."
+              }
             />
           </div>
         </div>
-        <Footer />
+
+        <div class="footer">
+          <Footer />
+        </div>
       </div>
     );
   }
